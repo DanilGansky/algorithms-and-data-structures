16c7b4710ecd876813e13064b2428fc3f0d947dc
danikg <danya.gansky@gmail.com>
2020-08-29 15:14:05 +0300

    Updated imports for tests

---
+++ algorithms/__init__.py
@@ -0,0 +1,5 @@
+from .sorting.insertion_sort import tests as insertion_sort_tests
+from .sorting.quicksort import tests as quicksort_tests
+from .sorting.merge_sort import tests as merge_sort_tests
+from .sorting.selection_sort import tests as selection_sort_tests
+from .sorting.radix_sort import tests as radix_sort_tests
