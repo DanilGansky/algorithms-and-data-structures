f8cda5c8dbfee7aa153a46cf0cc2a1031d13369c
danikg <danya.gansky@gmail.com>
2020-08-29 14:59:33 +0300

    Added the ability to run specific tests

--- algorithms/sorting/__init__.py
+++ algorithms/sorting/__init__.py
@@ -4,6 +4,12 @@ from .merge_sort.merge_sort import merge_sort
 from .selection_sort.selection_sort import selection_sort
 from .radix_sort.radix_sort import radix_sort
 
+from .insertion_sort import tests as insertion_sort_tests
+from .quicksort import tests as quicksort_tests
+from .merge_sort import tests as merge_sort_tests
+from .selection_sort import tests as selection_sort_tests
+from .radix_sort import tests as radix_sort_tests
+
 
 __all__ = (insertion_sort, quicksort, merge_sort,
            selection_sort, radix_sort)
---
+++ data_structures/__init__.py
@@ -0,0 +1,4 @@
+from .linked_lists.singly_linked_list import tests as singly_linked_list_tests
+from .linked_lists.doubly_linked_list import tests as doubly_linked_list_tests
+from .stack import tests as stack_tests
+from .queue import tests as queue_tests
